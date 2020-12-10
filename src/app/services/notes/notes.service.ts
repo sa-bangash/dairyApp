a265471a64bd8fd8e5df743371818fdc92022c37
shahid bangash <shahid.bangash@hotmail.com>
2020-12-10 19:47:47 +0500

    changing default video

--- src/app/services/notes/notes.service.ts
+++ src/app/services/notes/notes.service.ts
@@ -10,14 +10,14 @@ export class NotesService {
     text: 'Today i gone to market.',
     date: new Date(),
     imageUrl: 'https://scontent.fpew3-1.fna.fbcdn.net/v/t1.0-1/p480x480/26239199_949372878548131_4224877183970466593_n.jpg?_nc_cat=103&ccb=2&_nc_sid=7206a8&_nc_ohc=pzKWM5eCZwgAX85B9vG&_nc_ht=scontent.fpew3-1.fna&tp=6&oh=e9a0c2ed8c9ba942656f5722fe242558&oe=5FF7D4B4',
-    videoUrl: 'https://youtu.be/QZMf2zkKoF8',
+    videoUrl: 'https://youtu.be/f3NWvUV8MD8',
   },
   {
     id: 2,
     text: 'This is anther notes.',
     date: addDays(new Date(), 3),
     imageUrl: 'https://scontent.fpew3-1.fna.fbcdn.net/v/t1.0-1/p480x480/26239199_949372878548131_4224877183970466593_n.jpg?_nc_cat=103&ccb=2&_nc_sid=7206a8&_nc_ohc=pzKWM5eCZwgAX85B9vG&_nc_ht=scontent.fpew3-1.fna&tp=6&oh=e9a0c2ed8c9ba942656f5722fe242558&oe=5FF7D4B4',
-    videoUrl: 'https://youtu.be/QZMf2zkKoF8',
+    videoUrl: 'https://youtu.be/f3NWvUV8MD8',
 
   }];
   constructor() { }
