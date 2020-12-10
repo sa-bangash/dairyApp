c3c493a818b110f52d491fa78d9c554ab63f89da
shahid bangash <shahid.bangash@hotmail.com>
2020-12-10 16:56:34 +0500

    show image

--- src/app/components/note-form/note-form.component.ts
+++ src/app/components/note-form/note-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { NotesService } from 'src/app/services/notes/notes.service';
 
@@ -15,14 +15,19 @@ export class NoteFormComponent implements OnInit {
     this.form = this.fb.group({
       id: '',
       text: ['', [Validators.required]],
-      imageUrl: '',
-      videoUrl: '',
+      imageUrl: 'https://scontent.fpew3-1.fna.fbcdn.net/v/t1.0-1/p480x480/26239199_949372878548131_4224877183970466593_n.jpg?_nc_cat=103&ccb=2&_nc_sid=7206a8&_nc_ohc=pzKWM5eCZwgAX85B9vG&_nc_ht=scontent.fpew3-1.fna&tp=6&oh=e9a0c2ed8c9ba942656f5722fe242558&oe=5FF7D4B4',
+      videoUrl: 'https://youtu.be/QZMf2zkKoF8',
       date: new Date()
     });
   }
 
+  get ImageUrlCtrl(): FormControl {
+    return this.form.get('imageUrl') as FormControl;
+  }
+
   ngOnInit(): void {
   }
+
   onSubmit(): void {
     if (this.form.valid) {
       this.service.add(this.form.value);
